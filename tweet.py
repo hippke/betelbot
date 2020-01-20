95d0386b8de6d367006267e317139a03b03bacd4
Michael Hippke <hippke@users.noreply.github.com>
2020-01-20 21:59:19 +0100

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -2,10 +2,9 @@ import numpy as np
 from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
 from wotan import flatten
-#from twython import Twython
-#from auth import consumer_key, consumer_secret, access_token, access_token_secret
+from twython import Twython
+from auth import consumer_key, consumer_secret, access_token, access_token_secret
 import requests
-import pandas as pd
 
 
 def tweet(text, image):
@@ -16,22 +15,23 @@ def tweet(text, image):
     print("Done.")
 
 
-def make_plot(days_ago, mag):
+def make_plot(days_ago, dates, mag):
     print('Making plot...')
-    time_span = np.max(date) - np.min(date)
+    time_span = np.max(dates) - np.min(dates)
     flatten_lc, trend_lc = flatten(
         days_ago,
         mag,
         method='lowess',
-        window_length=time_span/3,
+        window_length=time_span/5,
         return_trend=True,
         )
-    plt.scatter(-days_ago, mag, s=5, color='blue', alpha=0.5)
+    plt.scatter(days_ago, mag, s=5, color='blue', alpha=0.5)
     plt.xlabel('Days before today')
     plt.ylabel('Visual magnitude')
-    plt.plot(-days_ago, trend_lc, color='red', linewidth=1)
+    plt.plot(days_ago, trend_lc, color='red', linewidth=1)
     plt.gca().invert_yaxis()
-    plt.savefig(plot_file, bbox_inches='tight')
+    plt.gca().invert_xaxis()
+    plt.savefig(plot_file, bbox_inches='tight', dpi=300)
     print('Done.')
 
 
@@ -55,10 +55,8 @@ def build_string(days_ago, mag):
 
         if diff > 0:
             changeword = 'dimmer'
-        elif diff < 0:
-            changeword = 'brighter'
         else:
-            changeword = 'constant'
+            changeword = 'brighter'
 
         mag_text = "My visual mag from last night was " + \
             str(format(mean_last24hrs, '.2f')) + \
@@ -66,94 +64,45 @@ def build_string(days_ago, mag):
             str(n_obs_last24hrs) + \
             ' observations). '
 
-        change_text = 'It is ' + \
-            format(diff, '.2f') + \
+        change_text = 'That is ' + \
+            format(abs(diff), '.2f') + \
             ' mag ' + \
             changeword + \
-            ' than the avg of the 5 previous nights (avg of ' + \
+            ' than the avg of the 5 previous nights (n=' + \
             str(n_obs_last1_6_days) + \
-            ' observations). '
+            ', ' + \
+            format(abs(sigma), '.1f') + \
+            'σ). #Betelgeuse'
 
-        sig_text = 'The significance of this change is ' + \
-            format(sigma, '.1f') + \
-            ' sigma.'
-
-        text = mag_text + change_text + sig_text
+        text = mag_text + change_text
         print(text)
         return text
 
 
-class HTMLTableParser:
-           
-    def parse_url(self, url):
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'lxml')
-        return [(table['id'],self.parse_html_table(table))\
-                for table in soup.find_all('table')]  
-
-    def parse_html_table(self, table):
-        n_columns = 0
-        n_rows=0
-        column_names = []
-
-        # Find number of rows and columns
-        # we also find the column titles if we can
-        for row in table.find_all('tr'):
-            
-            # Determine the number of rows in the table
-            td_tags = row.find_all('td')
-            if len(td_tags) > 0:
-                n_rows+=1
-                if n_columns == 0:
-                    # Set the number of columns for our table
-                    n_columns = len(td_tags)
-                    
-            # Handle column names if we find them
-            th_tags = row.find_all('th') 
-            if len(th_tags) > 0 and len(column_names) == 0:
-                for th in th_tags:
-                    column_names.append(th.get_text())
-
-        # Safeguard on Column Titles
-        if len(column_names) > 0 and len(column_names) != n_columns:
-            raise Exception("Column titles do not match the number of columns")
-
-        columns = column_names if len(column_names) > 0 else range(0,n_columns)
-        df = pd.DataFrame(columns = columns,
-                          index= range(0,n_rows))
-        row_marker = 0
-        for row in table.find_all('tr'):
-            column_marker = 0
-            columns = row.find_all('td')
-            for column in columns:
-                df.iat[row_marker,column_marker] = column.get_text()
-                column_marker += 1
-            if len(columns) > 0:
-                row_marker += 1
-                
-        # Convert to float if possible
-        for col in df:
-            try:
-                df[col] = df[col].astype(float)
-            except ValueError:
-                pass
-        
-        return df
-
 plot_file = 'plot.png'
-filename = 'betelflux.csv'
 url = 'https://www.aavso.org/apps/webobs/results/?star=betelgeuse&num_results=200&obs_types=vis'
 
-hp = HTMLTableParser()
-table = hp.parse_url(url)[0][1]
-table.head()
-avg=table['Avg'].values
-print(avg)
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'html.parser')
+rows = soup.select('tbody tr')
+dates = []
+mags = []
+for row in rows:
+    string = '' + row.text
+    string = string.split('\n')
+    try:
+        date = float(string[3])
+        mag = float(string[5])
+        dates.append(date)
+        mags.append(mag)
+    except:
+        pass
+dates = np.array(dates)
+mags = np.array(mags)
 
-date, mag = np.loadtxt(filename, unpack=True)
-days_ago = np.max(date) - date
-text = build_string(days_ago, mag)
+#dates, mags = np.loadtxt(filename, unpack=True)
+days_ago = np.max(dates) - dates
+text = build_string(days_ago, mags)
 if text is not None:
-    make_plot(days_ago, mag)
+    make_plot(days_ago, dates, mags)
     tweet(text, plot_file)
-
