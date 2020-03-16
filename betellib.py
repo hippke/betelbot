1d4bebee895a91884c019b3f3993356590678d6f
aaronx0607 <43068290+aaronx0607@users.noreply.github.com>
2020-03-17 00:07:02 +0800

    Update betellib.py

--- betellib.py
+++ betellib.py
@@ -5,19 +5,9 @@ from twython import Twython
 from bs4 import BeautifulSoup
 from astropy.stats import biweight_location
 
-
-consumer_key = os.environ.get('consumer_key')
-consumer_secret = os.environ.get('consumer_secret')
 access_token = os.environ.get('access_token')
 access_token_secret = os.environ.get('access_token_secret')
 
-
-def tweet(text, image):
-    print('Tweeting...')
-    twitter = Twython(consumer_key, consumer_secret, access_token, access_token_secret)
-    response = twitter.upload_media(media=open(image, 'rb'))
-    twitter.update_status(status=text, media_ids=[response['media_id']])
-    print("Done.")
 
 
 def build_string(days_ago, mag):
